c2165e9d4190a6de18e13f55bebb9f701020e00e
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-10-08 15:21:47 +0700

    feat: add tranpose test

--- src/lib/Matrix.java
+++ src/lib/Matrix.java
@@ -8,15 +8,18 @@ public class Matrix {
     int cols;
 
     // ==== ini buat nge tes co ==== //
-    // public static void main(String[] args) {
-    // Matrix m1 = new Matrix(3, 3);
-    // m1.readMatrix();
-    // System.out.println();
-    // m1.printMatrix();
-    // System.out.println();
-    // m1.plusKRow(0, 2, 1);
-    // m1.printMatrix();
-    // }
+    public static void main(String[] args) {
+        Matrix m1 = new Matrix(3, 3);
+        m1.readMatrix();
+        System.out.println();
+        m1.printMatrix();
+        System.out.println();
+        m1.plusKRow(0, 2, 1);
+        m1.printMatrix();
+        System.out.println();
+        m1.transpose();
+        m1.printMatrix();
+    }
 
     // Konstruktor
     public Matrix(int _rows, int _cols) {
