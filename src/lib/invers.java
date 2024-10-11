36e7d6a38be773233f8af5b93605d462f025106a
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-10-11 07:39:22 +0700

    feat: add inversMatrix test

--- src/lib/invers.java
+++ src/lib/invers.java
@@ -1,5 +1,16 @@
 package lib;
 
+import java.util.Scanner;
+
 public class invers {
+    // ga punya atribut
+
+    // buat nge tes
+    public static void main(String[] args) {
+        Matrix m = new Matrix(3, 3);
+        Scanner scanner = new Scanner(System.in);
+        m.readMatrix(scanner);
+        showInversOBE(m);
+    }
 
 }
