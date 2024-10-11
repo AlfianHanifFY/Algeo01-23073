58a07883ed4bd9a6cde37c87251460e21130f5be
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-10-11 08:53:48 +0700

    feat: add gaussSolution procedure

--- src/lib/SPL.java
+++ src/lib/SPL.java
@@ -96,4 +96,55 @@ public class SPL {
         }
         return false;
     }
+
+    public static void gaussSolution(Matrix m) {
+        int i, j, n, ex = 0;
+        double[] res = new double[m.getCol() - 1];
+        double val;
+        m.generateEselon();
+        m.printMatrix();
+        if (isNoSulution(m)) {
+            System.out.println("Tidak ada solusi ! ");
+        } else if (isLotSolution(m)) {
+            System.out.println("Banyak Solusi : ");
+            m.generateEselonReduksi();
+            for (i = 0; i < m.getCol() - 1; i++) {
+                n = i - ex;
+                if (m.rowLength(n) == m.getCol()) {
+                    System.out.print("x" + (n + 1) + " = " + (char) (97 + i));
+                } else if (i != m.rowLength(n)) {
+                    System.out.print("x" + (n + 1) + " = " + (char) (97 + i));
+                    ex += 1;
+                } else {
+                    val = m.getElmt(n, m.getCol() - 1);
+                    System.out.print("x" + (m.rowLength(n) + 1) + " = " + val);
+                    for (j = m.rowLength(n) + 1; j < m.getCol() - 1; j++) {
+                        if (m.getElmt(n, j) != 0) {
+                            val = m.getElmt(n, j) * (-1);
+                            if (val > 0) {
+                                System.out.print(" + " + val + "" + (char) (97 + j));
+                            } else {
+                                System.out.print(" - " + (val * -1) + "" + (char) (97 + j));
+                            }
+
+                        }
+                    }
+
+                }
+                System.out.println();
+            }
+        } else if (isUnique(m)) {
+            System.out.println("Solusi unik : ");
+            for (i = m.getRow() - 1; i >= 0; i--) {
+                val = 0;
+                for (j = m.rowLength(i) + 1; j < m.getCol() - 1; j++) {
+                    val -= m.getElmt(i, j) * res[j];
+                }
+                res[i] = m.getElmt(i, m.getCol() - 1) + val;
+            }
+            for (i = 0; i < m.getCol() - 1; i++) {
+                System.out.println("x" + (i + 1) + " = " + res[i]);
+            }
+        }
+    }
 }
