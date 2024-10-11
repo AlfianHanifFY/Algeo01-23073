38cace2fe66638b2390f91bd43726bc1c1413ce5
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-10-11 14:35:26 +0700

    feat: add determinanReduksi test

--- src/lib/determinan.java
+++ src/lib/determinan.java
@@ -1,5 +1,14 @@
 package lib;
 
+import java.util.Scanner;
+
 public class determinan {
+    public static void main(String[] args) {
+        Matrix m = new Matrix(3, 3);
+        int x;
+        Scanner scanner = new Scanner(System.in);
+        m.readMatrix(scanner);
+        System.out.println(determinanReduksi(m));
+    }
 
 }
