b1c6a9ba54a15fbd4681dd4a8217b0b7072e56e1
carllix <carloangkisan21@gmail.com>
2024-10-14 10:31:23 +0700

    feat: add BicubicSplineInterpolation Algorithm

--- src/lib/BicubicSplineInterpolation.java
+++ src/lib/BicubicSplineInterpolation.java
@@ -1,5 +1,8 @@
 package lib;
 
+import java.lang.Math;
+
+
 public class BicubicSplineInterpolation {
     
     public static void main(String[] args) {
@@ -7,12 +10,111 @@ public class BicubicSplineInterpolation {
         String fileName = IO.promptFileName();
         IO f = new IO(fileName);
         double[] temp = f.readBicubicSplineData();
-        
-        // Print each element in the double array
-        System.out.println("Bicubic Spline Data:");
-        for (double value : temp) {
-            System.out.print(value);
-            System.out.print(" ");
+        Matrix M = new Matrix(16, 1);
+        Matrix X = new Matrix(16, 16);
+        Matrix res = new Matrix(16, 1);
+        double a, b, val = 0;
+
+        // Construct Matrix X
+        constructX(X);
+
+        // Read input from file
+        for (int i = 0; i < 16; i++) {
+            M.setElMT(i, 0, temp[i]);
+        }
+        a = temp[16];
+        b = temp[17];
+
+        // Inverse X and multiply with M
+        Matrix invX = invers.getInversOBE(X);
+        res = Matrix.multiplyMatrix(invX, M);
+
+        // solve for x and y
+        int idx = 0;
+        for (int j = 0; j < 4; j++) {
+            for (int i = 0; i < 4; i++) {
+                val += res.getElmt(idx, 0) * Math.pow(a, i) * Math.pow(b, j);
+                idx++;
+            }
+        }
+        System.out.println();
+        System.out.println("f(" + a + "," + b + ") = " + val);
+        System.out.println();
+        System.out.println(">> Apakah ingin menyimpan keluaran?");
+        System.out.println(">> 1. Ya");
+        System.out.println(">> 2. Tidak");
+        System.out.print(">> Masukkan pilihan: ");
+        int choice = IO.inputScanner.nextInt();
+        if (choice == 1){
+            String[] s = new String[1];
+            s[0] = "f(" + a + "," + b + ") = " + val;
+            f.writeStringArrayToFile(s);
+        }
+    }
+
+    // Construct Matrix X
+    public static void constructX(Matrix X) {
+        int row = 0, col = 0;
+        // f(x, y)
+        for (int y = 0; y < 2; y++) {
+            for (int x = 0; x < 2; x++) {
+                col = 0;
+                for (int j = 0; j < 4; j++) {
+                    for (int i = 0; i < 4; i++) {
+                        X.setElMT(row, col, Math.pow(x, i) * Math.pow(y, j));
+                        col++;
+                    }
+                }
+                row++;
+            }
+        }
+        // fx(x, y);
+        for (int y = 0; y < 2; y++) {
+            for (int x = 0; x < 2; x++) {
+                col = 0;
+                for (int j = 0; j < 4; j++) {
+                    for (int i = 0; i < 4; i++) {
+                        if (i != 0)
+                            X.setElMT(row, col, i * Math.pow(x, i - 1) * Math.pow(y, j));
+                        else
+                            X.setElMT(row, col, 0);
+                        col++;
+                    }
+                }
+                row++;
+            }
+        }
+        // fy(x, y)
+        for (int y = 0; y < 2; y++) {
+            for (int x = 0; x < 2; x++) {
+                col = 0;
+                for (int j = 0; j < 4; j++) {
+                    for (int i = 0; i < 4; i++) {
+                        if (j != 0)
+                            X.setElMT(row, col, j * Math.pow(x, i) * Math.pow(y, j - 1));
+                        else
+                            X.setElMT(row, col, 0);
+                        col++;
+                    }
+                }
+                row++;
+            }
+        }
+        // fxy(x, y)
+        for (int y = 0; y < 2; y++) {
+            for (int x = 0; x < 2; x++) {
+                col = 0;
+                for (int j = 0; j < 4; j++) {
+                    for (int i = 0; i < 4; i++) {
+                        if (i != 0 && j != 0)
+                            X.setElMT(row, col, i * j * Math.pow(x, i - 1) * Math.pow(y, j - 1));
+                        else
+                            X.setElMT(row, col, 0);
+                        col++;
+                    }
+                }
+                row++;
+            }
         }
     }
 }
