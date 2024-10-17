aa953fe5d035d20f19f7a940c138781aa0d7af26
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-10-17 12:52:41 +0700

    chore: refactor gaussSolution method to String[]

--- src/lib/SPL.java
+++ src/lib/SPL.java
@@ -134,44 +134,57 @@ public class SPL {
         return false;
     }
 
-    public static void gaussSolution(Matrix m) {
+    public static String[] gaussSolution(Matrix m) {
         // SOLUSI BANYAKNYA MASIH KEOS -- NANTI DI CEK
         int i, j, n, ex = 0;
         double[] res = new double[m.getCol() - 1];
         double val;
         m.generateEselon();
         m.printMatrix();
+
         if (isNoSulution(m)) {
+            String[] txt = new String[1];
+            txt[0] = "Tidak ada solusi ! ";
             System.out.println("Tidak ada solusi ! ");
+            return txt;
         } else if (isLotSolution(m)) {
+            String[] txt = new String[m.getCol()];
             System.out.println("Banyak Solusi : ");
             m.generateEselonReduksi();
             for (i = 0; i < m.getCol() - 1; i++) {
                 n = i - ex;
                 if (m.rowLength(n) == m.getCol()) {
+                    txt[n] = String.format("x" + (n + 1) + " = " + (char) (97 + i));
                     System.out.print("x" + (n + 1) + " = " + (char) (97 + i));
                 } else if (i != m.rowLength(n)) {
+                    txt[n] = String.format("x" + (n + 1) + " = " + (char) (97 + i));
                     System.out.print("x" + (n + 1) + " = " + (char) (97 + i));
                     ex += 1;
                 } else {
                     val = m.getElmt(n, m.getCol() - 1);
+                    String restxt = new String();
+                    restxt = String.format("x" + (m.rowLength(n) + 1) + " = " + val);
                     System.out.print("x" + (m.rowLength(n) + 1) + " = " + val);
                     for (j = m.rowLength(n) + 1; j < m.getCol() - 1; j++) {
                         if (m.getElmt(n, j) != 0) {
                             val = m.getElmt(n, j) * (-1);
                             if (val > 0) {
+                                restxt += String.format(" + " + val + "" + (char) (97 + j));
                                 System.out.print(" + " + val + "" + (char) (97 + j));
                             } else {
+                                restxt += String.format(" - " + val + "" + (char) (97 + j));
                                 System.out.print(" - " + (val * -1) + "" + (char) (97 + j));
                             }
 
                         }
                     }
-
+                    txt[n] = restxt;
                 }
                 System.out.println();
             }
+            return txt;
         } else if (isUnique(m)) {
+            String[] txt = new String[m.getRow()];
             System.out.println("Solusi unik : ");
             for (i = m.getRow() - 1; i >= 0; i--) {
                 val = 0;
@@ -181,9 +194,12 @@ public class SPL {
                 res[i] = m.getElmt(i, m.getCol() - 1) + val;
             }
             for (i = 0; i < m.getCol() - 1; i++) {
+                txt[i] = String.format("x" + (i + 1) + " = " + res[i]);
                 System.out.println("x" + (i + 1) + " = " + res[i]);
             }
+            return txt;
         }
+        return null;
     }
 
     // metode balikan
