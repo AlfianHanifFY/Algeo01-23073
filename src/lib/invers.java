1a4d8a6f1e4268960d475d4270b5c217632eec5a
Alfian Hanif FY <alfian.yustanto@gmail.com>
2024-10-19 11:38:10 +0700

    chore: remove printmatrix in getinverseAdjoin

--- src/lib/invers.java
+++ src/lib/invers.java
@@ -67,7 +67,6 @@ public class invers {
         // A' = 1/det(A) * adj(A)
         Matrix.copyMatrix(m, cm);
         M = Matrix.multiplyMatrixByConst(determinan.getAdjoin(m), (1 / determinan.determinanReduksi(cm)));
-        M.printMatrix();
         return M;
     }
 
